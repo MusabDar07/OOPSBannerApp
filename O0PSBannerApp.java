8e4f8c29ca72f4ddabf07e3577337d273c17e4e7
MusabDar07 <musabdar57@gamil.com>
2026-02-23 15:16:39 +0530

    i printed oops using *

---
+++ O0PSBannerApp.java
@@ -0,0 +1,14 @@
+public class O0PSBannerApp {
+	public static void main(String[] args) {
+		public class OOPSBanner {
+    public static void main(String[] args) {
+        System.out.println(String.join("  ", "  ***  ", "  ***  ", " ****  ", " ***** "));
+        System.out.println(String.join("  ", " *   * ", " *   * ", "*    * ", "*      "));
+        System.out.println(String.join("  ", "*     *", "*     *", "*    * ", "*      "));
+        System.out.println(String.join("  ", "*     *", "*     *", "****   ", " ***** "));
+        System.out.println(String.join("  ", "*     *", "*     *", "*      ", "      *"));
+        System.out.println(String.join("  ", " *   * ", " *   * ", "*      ", "*     *"));
+        System.out.println(String.join("  ", "  ***  ", "  ***  ", "*      ", " ***** "));
+    }
+}
+```
--- oopsbanner.java
+++
@@ -1,4 +0,0 @@
-public class O0PSBannerApp {
-	public static void main(String[] args) {
-		System.out.printin("00PS");
-}}
