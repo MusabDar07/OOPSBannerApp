89e88b5244b0eac278675b5c4a2ad5236f351120
MusabDar07 <musabdar57@gamil.com>
2026-03-02 13:23:49 +0530

    made banner using string array and loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,14 +2,50 @@ public class OOPSBannerApp {
     public static void main(String[] args) {
         public class OOPSBanner {
     public static void main(String[] args) {
-        System.out.println(String.join("  ", "  ***  ", "  ***  ", " ****  ", " ***** "));
-        System.out.println(String.join("  ", " *   * ", " *   * ", "*    * ", "*      "));
-        System.out.println(String.join("  ", "*     *", "*     *", "*    * ", "*      "));
-        System.out.println(String.join("  ", "*     *", "*     *", "****   ", " ***** "));
-        System.out.println(String.join("  ", "*     *", "*     *", "*      ", "      *"));
-        System.out.println(String.join("  ", " *   * ", " *   * ", "*      ", "*     *"));
-        System.out.println(String.join("  ", "  ***  ", "  ***  ", "*      ", " ***** "));
+        System.out.println(String.jpublic class OOPSBanner {
+    public static void main(String[] args) {
+
+        String[] O = {
+            "  ***  ",
+            " *   * ",
+            "*     *",
+            "*     *",
+            "*     *",
+            " *   * ",
+            "  ***  "
+        };
+
+        String[] P = {
+            " **** ",
+            "*    *",
+            "*    *",
+            "****  ",
+            "*     ",
+            "*     ",
+            "*     "
+        };
+
+        String[] S = {
+            " ***** ",
+            "*      ",
+            "*      ",
+            " ***** ",
+            "      *",
+            "      *",
+            " ***** "
+        };
+
+        String[][] letters = { O, O, P, S };
+
+        for (int row = 0; row < 7; row++) {
+            StringBuilder line = new StringBuilder();
+            for (int col = 0; col < letters.length; col++) {
+                if (col > 0) line.append("  ");
+                line.append(letters[col][row]);
+            }
+            System.out.println(line);
+        }
     }
 }
-    }
-}
+```
+
