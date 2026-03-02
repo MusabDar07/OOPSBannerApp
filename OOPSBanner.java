3f43efac27e6b257680a507a73b8050695e1d899
MusabDar07 <musabdar57@gamil.com>
2026-03-02 13:49:17 +0530

    oops banner made using maps for character patterns and render via functions

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -1,92 +1,81 @@
+import java.util.*;
+
 public class OOPSBanner {
 
-    // Class to store a character and its pattern
-    static class CharPattern {
-        char letter;
-        String[] pattern;
+    // Build and return the pattern map
+    static Map<Character, String[]> buildPatternMap() {
+        Map<Character, String[]> map = new LinkedHashMap<>();
 
-        // Constructor
-        CharPattern(char letter, String[] pattern) {
-            this.letter = letter;
-            this.pattern = pattern;
-        }
+        map.put('O', new String[]{
+            "  ***  ",
+            " *   * ",
+            "*     *",
+            "*     *",
+            "*     *",
+            " *   * ",
+            "  ***  "
+        });
+        map.put('P', new String[]{
+            " **** ",
+            "*    *",
+            "*    *",
+            "****  ",
+            "*     ",
+            "*     ",
+            "*     "
+        });
+        map.put('S', new String[]{
+            " ***** ",
+            "*      ",
+            "*      ",
+            " ***** ",
+            "      *",
+            "      *",
+            " ***** "
+        });
+        map.put(' ', new String[]{
+            "       ",
+            "       ",
+            "       ",
+            "       ",
+            "       ",
+            "       ",
+            "       "
+        });
 
-        // Method to get a specific row of the pattern
-        String getRow(int row) {
-            return pattern[row];
-        }
-
-        // Method to get the height of the pattern
-        int getHeight() {
-            return pattern.length;
-        }
+        return map;
     }
 
-    // Function to get CharPattern for each character
-    static CharPattern getCharPattern(char c) {
-        switch (c) {
-            case 'O': return new CharPattern('O', new String[]{
-                "  ***  ",
-                " *   * ",
-                "*     *",
-                "*     *",
-                "*     *",
-                " *   * ",
-                "  ***  "
-            });
-            case 'P': return new CharPattern('P', new String[]{
-                " **** ",
-                "*    *",
-                "*    *",
-                "****  ",
-                "*     ",
-                "*     ",
-                "*     "
-            });
-            case 'S': return new CharPattern('S', new String[]{
-                " ***** ",
-                "*      ",
-                "*      ",
-                " ***** ",
-                "      *",
-                "      *",
-                " ***** "
-            });
-            default: return new CharPattern(' ', new String[]{
-                "       ",
-                "       ",
-                "       ",
-                "       ",
-                "       ",
-                "       ",
-                "       "
-            });
-        }
+    // Lookup pattern for a character from the map
+    static String[] getPattern(char c, Map<Character, String[]> patternMap) {
+        return patternMap.getOrDefault(c, patternMap.get(' '));
     }
 
-    // Function to build list of CharPattern from a word
-    static CharPattern[] buildPatterns(String word) {
-        CharPattern[] patterns = new CharPattern[word.length()];
+    // Build patterns array for the word
+    static String[][] buildPatterns(String word, Map<Character, String[]> patternMap) {
+        String[][] patterns = new String[word.length()][];
         for (int i = 0; i < word.length(); i++) {
-            patterns[i] = getCharPattern(word.charAt(i));
+            patterns[i] = getPattern(word.charAt(i), patternMap);
         }
         return patterns;
     }
 
-    // Function to print the banner
-    static void printBanner(String word) {
-        CharPattern[] patterns = buildPatterns(word);
-        for (int row = 0; row < patterns[0].getHeight(); row++) {
+    // Print the banner for the given word
+    static void printBanner(String word, Map<Character, String[]> patternMap) {
+        String[][] patterns = buildPatterns(word, patternMap);
+        int height = patterns[0].length;
+        for (int row = 0; row < height; row++) {
             StringBuilder line = new StringBuilder();
             for (int col = 0; col < patterns.length; col++) {
                 if (col > 0) line.append("  ");
-                line.append(patterns[col].getRow(row));
+                line.append(patterns[col][row]);
             }
             System.out.println(line);
         }
     }
 
     public static void main(String[] args) {
-        printBanner("OOPS");
+        Map<Character, String[]> patternMap = buildPatternMap();
+        printBanner("OOPS", patternMap);
     }
 }
