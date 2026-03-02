ad24434a542ca7521cde0308041b33d57c64f1a5
MusabDar07 <musabdar57@gamil.com>
2026-03-02 13:13:03 +0530

    made banner using string join

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,15 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
+        public class OOPSBanner {
+    public static void main(String[] args) {
+        System.out.println(String.join("  ", "  ***  ", "  ***  ", " ****  ", " ***** "));
+        System.out.println(String.join("  ", " *   * ", " *   * ", "*    * ", "*      "));
+        System.out.println(String.join("  ", "*     *", "*     *", "*    * ", "*      "));
+        System.out.println(String.join("  ", "*     *", "*     *", "****   ", " ***** "));
+        System.out.println(String.join("  ", "*     *", "*     *", "*      ", "      *"));
+        System.out.println(String.join("  ", " *   * ", " *   * ", "*      ", "*     *"));
+        System.out.println(String.join("  ", "  ***  ", "  ***  ", "*      ", " ***** "));
+    }
 }
-
+    }
 }
