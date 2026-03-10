50fdead718d6a3a318593e2ab9f6c9bab3f2a7d8
MusabDar07 <musabdar57@gamil.com>
2026-03-10 13:53:24 +0530

    features/uc2

---
+++ OOPSBannerApp/Uc2.java
@@ -0,0 +1,16 @@
+package OOPSBannerApp;
+
+public class Uc2 {
+    
+    public static void main(String[] args) {
+        // O
+        System.out.println("  ***    ***    ***   *****   ***  ");
+        System.out.println(" *   *  *   *  *   *  *       *   * ");
+        System.out.println("*     **     **     * *       *    *");
+        System.out.println("*     **     **     * ****    *****");
+        System.out.println("*     **     **     * *       *");
+        System.out.println(" *   *  *   *  *   *  *       *");
+        System.out.println("  ***    ***    ***   *****   *");
+    }
+}
+
