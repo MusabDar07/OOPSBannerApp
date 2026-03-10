a6086a247995953858ee8412c122e9a289fd5299
MusabDar07 <musabdar57@gamil.com>
2026-03-10 14:11:15 +0530

    featureuc3

--- OOPSBannerApp/Uc2.java
+++ OOPSBannerApp/Uc2.java
@@ -1,16 +1,12 @@
 package OOPSBannerApp;
-
 public class Uc2 {
     
+
     public static void main(String[] args) {
-        // O
-        System.out.println("  ***    ***    ***   *****   ***  ");
-        System.out.println(" *   *  *   *  *   *  *       *   * ");
-        System.out.println("*     **     **     * *       *    *");
-        System.out.println("*     **     **     * ****    *****");
-        System.out.println("*     **     **     * *       *");
-        System.out.println(" *   *  *   *  *   *  *       *");
-        System.out.println("  ***    ***    ***   *****   *");
+        System.out.println("  ***    ***   ****   ****  ");
+        System.out.println(" *   *  *   *  *   *  *     ");
+        System.out.println(" *   *  *   *  ****   ****  ");
+        System.out.println(" *   *  *   *  *          * ");
+        System.out.println("  ***    ***   *       ****  ");
     }
 }
-
---
+++ OOPSBannerApp/uc3.java
@@ -0,0 +1,44 @@
+package OOPSBannerApp;
+
+public class uc3 {
+    public class OOPSBanner {
+    public static void main(String[] args) {
+
+        String r1 = String.join("   ",
+             " ***** ",
+                " ***** ",
+                "****** ",
+                " ***** ");
+
+        String r2 = String.join("   ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     ");
+
+        String r3 = String.join("   ",
+                "*     *",
+                "*     *",
+                "****** ",
+                " ***** ");
+
+        String r4 = String.join("   ",
+                "*     *",
+                "*     *",
+                "*      ",
+                "      *");
+
+        String r5 = String.join("   ",
+                " ***** ",
+                " ***** ",
+                "*      ",
+                " ***** ");
+
+        System.out.println(r1);
+        System.out.println(r2);
+        System.out.println(r3);
+        System.out.println(r4);
+        System.out.println(r5);
+    }
+}
+}
